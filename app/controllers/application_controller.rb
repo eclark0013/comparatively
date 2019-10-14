a25f16fa76f5f2967af96a0033e8e8e1c389d909
Eric Clark <eclark0013@gmail.com>
2019-10-14 17:02:32 -0400

    Debug sessions, add logout link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,67 +2,68 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "secret"
+    end
 
-  enable :sessions
+    get '/session' do
+        session
+    end
 
-  get '/' do
-    erb :'/login'
-  end
+    get '/' do
+        erb :'/login'
+    end
 
-  get '/signup' do
-    if logged_in?
-        @user = User.find(session[:user_id])
+    get '/signup' do
+        if logged_in?
+            @user = User.find(session[:user_id])
+            redirect "/users/#{@user.id}"
+        end
+        erb :'/users/create_user'
+    end
+
+    post '/signup' do
+        #create functionality to not allow two users to have the same username
+        @user = User.create(params)
         redirect "/users/#{@user.id}"
     end
-    erb :'/users/create_user'
-  end
 
-  post '/signup' do
-    binding.pry
-    #create functionality to not allow two users to have the same username
-    @user = User.create(params)
-    redirect "/users/#{@user.id}"
-  end
-
-  get '/users/:id' do
-    @user = User.find(params[:id])
-    # add a link to make a new rating
-    # add a link to see all subjects
-    erb :'/users/show_user'
-  end
-
-  get '/login' do
-    if logged_in?
-        @user = User.find(session[:user_id])
-        redirect "/users/#{@user.id}"
-    else
-        erb :'/users/login'
+    get '/login' do
+        if logged_in?
+            @user = current_user
+            redirect "/users/#{@user.id}"
+        else
+            erb :'/users/login'
+        end
     end
-  end
 
-  post '/login' do
-    # binding.pry # figure out how to authenticate on login with has_secure_password 
-    params
-    user = User.find_by(username: params[:username]).authenticate(params[:password])
-    if !!user
-      session[:user_id] = user.id
-      redirect "/users/#{user.id}"
-    else
-      redirect '/login'
+    post '/login' do
+        # binding.pry # figure out how to authenticate on login with has_secure_password 
+        user = User.find_by(username: params[:username]).authenticate(params[:password])
+        if !!user
+            session[:user_id] = user.id
+            redirect "/users/#{user.id}"
+        else
+            redirect '/login'
+        end
     end
-  end
 
-  get '/subjects/:id' do
-    @subject = Subject.find(params[:id])
-    # add a link to make a new rating (or edit your rating if you have already rated this)
-    erb :'/subjects/show_subject'
-  end
+    get '/logout' do
+        session[:user_id] = nil
+        redirect :'/login'
+    end
 
-  helpers do
+    get '/users/:id' do
+        @user = User.find(params[:id])
+        # add a link to make a new rating
+        # add a link to see all subjects
+        erb :'/users/show_user'
+    end
+
+    helpers do
         def logged_in?
             !!session[:user_id]
         end
