6b9bbb86f2a01054aa2231ec4ed84c1fee6293d1
Eric Clark <eclark0013@gmail.com>
2019-10-16 18:04:36 -0400

    Validate for presence of score on rating with new #average_score error

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ActiveRecord::Base
     belongs_to :user
     belongs_to :subject
+    validates :score, presence: true
 
     def score_difference
         self.score - self.user.average_score
